a4c0fee7083eb67a154d2af39036c70075c0a3c9
shaw <635776247@qq.com>
2018-08-17 19:56:48 +0800

    Commit

--- component-thread/src/test/java/org/shaw/task/StandardThreadExecutorTests.java
+++ component-thread/src/test/java/org/shaw/task/StandardThreadExecutorTests.java
@@ -103,4 +103,97 @@ public class StandardThreadExecutorTests {
         System.out.println("==================");
     }
 
+    @Test
+    public void TestListenableFuture() throws Exception {
+        StandardThreadExecutor executor = new StandardThreadExecutor(1, 1);
+
+        executor.setBefore((r, t) -> System.out.println("运行任务：" + r));
+        executor.setAfter(((r, t) -> System.out.println("任务结束：" + r)));
+
+        Future<String> task1 = null;
+        try {
+            task1 = executor.submitListenable(() -> {
+                System.out.println("测试1----开始");
+                Thread.sleep(10000);
+                System.out.println("测试1----结束");
+                return "测试1";
+            });
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+
+        Future<String> task2 = null;
+        try {
+            task2 = executor.submitListenable(() -> {
+                System.out.println("测试2----开始");
+                Thread.sleep(10000);
+                System.out.println("测试2----结束");
+                return "测试2";
+            });
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+
+        Future<String> task3 = null;
+        try {
+            task3 = executor.submitListenable(() -> {
+                System.out.println("测试3----开始");
+                Thread.sleep(10000);
+                System.out.println("测试3----结束");
+                return "测试3";
+            });
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            executor.submitListenable(() -> {
+                try {
+                    System.out.println("测试4----开始");
+                    Thread.sleep(10000);
+                    System.out.println("测试4----结束");
+                } catch (final Exception e) {
+                    e.printStackTrace();
+                }
+            });
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            executor.submitListenable(() -> {
+                try {
+                    System.out.println("测试5----开始");
+                    Thread.sleep(10000);
+                    System.out.println("测试5----结束");
+                } catch (final Exception e) {
+                    e.printStackTrace();
+                }
+            });
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            executor.submitListenable(() -> {
+                try {
+                    System.out.println("测试6----开始");
+                    Thread.sleep(10000);
+                    System.out.println("测试6----结束");
+                } catch (final Exception e) {
+                    e.printStackTrace();
+                }
+
+
+            });
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(task1.get());
+        System.out.println(task2.get());
+        System.out.println(task3.get());
+        System.out.println("==================");
+    }
+
 }
