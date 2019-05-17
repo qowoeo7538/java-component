f5cb17a1413d7285d1adfd86bcbf5d415761b45b
shaw.zhou <shaw.zhou@winway666.com>
2019-05-17 18:21:23 +0800

    Commit

---
+++ component-common/src/main/java/org/lucas/compiler/support/CtClassBuilder.java
@@ -0,0 +1,175 @@
+package org.lucas.compiler.support;
+
+import javassist.CannotCompileException;
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtField;
+import javassist.CtNewConstructor;
+import javassist.CtNewMethod;
+import javassist.LoaderClassPath;
+import javassist.NotFoundException;
+import org.lucas.util.ClassUtils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Author: shaw
+ * @Date: 2019/5/17 16:40
+ */
+public class CtClassBuilder {
+
+    /**
+     * 类名
+     */
+    private String className;
+
+    /**
+     * 所继承的父类,默认为 java.lang.Object.
+     */
+    private String superClassName = "java.lang.Object";
+
+    /**
+     * 类包
+     */
+    private List<String> imports = new ArrayList<>();
+
+    /**
+     * 类短名 --> 类全名
+     */
+    private Map<String, String> fullNames = new HashMap<>();
+
+    /**
+     * 接口类
+     */
+    private List<String> ifaces = new ArrayList<>();
+
+    /**
+     * 构造方法
+     */
+    private List<String> constructors = new ArrayList<>();
+
+    /**
+     * 字段属性
+     */
+    private List<String> fields = new ArrayList<>();
+
+    /**
+     * 方法
+     */
+    private List<String> methods = new ArrayList<>();
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getSuperClassName() {
+        return superClassName;
+    }
+
+    public void setSuperClassName(String superClassName) {
+        this.superClassName = getQualifiedClassName(superClassName);
+    }
+
+    public void addInterface(String iface) {
+        this.ifaces.add(getQualifiedClassName(iface));
+    }
+
+    public List<String> getInterfaces() {
+        return ifaces;
+    }
+
+    public List<String> getConstructors() {
+        return constructors;
+    }
+
+    public void addConstructor(String constructor) {
+        this.constructors.add(constructor);
+    }
+
+    public List<String> getFields() {
+        return fields;
+    }
+
+    public void addField(String field) {
+        this.fields.add(field);
+    }
+
+    public List<String> getMethods() {
+        return methods;
+    }
+
+    public void addMethod(String method) {
+        this.methods.add(method);
+    }
+
+    /**
+     * 添加引入类的信息.
+     *
+     * @param pkg 包信息
+     */
+    public void addImports(String pkg) {
+        int pi = pkg.lastIndexOf('.');
+        if (pi > 0) {
+            String pkgName = pkg.substring(0, pi);
+            this.imports.add(pkgName);
+            if (!pkg.endsWith(".*")) {
+                fullNames.put(pkg.substring(pi + 1), pkg);
+            }
+        }
+    }
+
+    public CtClass build(ClassLoader classLoader) throws NotFoundException, CannotCompileException {
+        ClassPool pool = new ClassPool(true);
+        pool.appendClassPath(new LoaderClassPath(classLoader));
+
+        // 创建类及父类
+        CtClass ctClass = pool.makeClass(className, pool.get(superClassName));
+
+        // 添加导入包
+        imports.forEach(pool::importPackage);
+
+        // 添加实现接口
+        for (String iface : ifaces) {
+            ctClass.addInterface(pool.get(iface));
+        }
+
+        // 添加构造方法
+        for (String constructor : constructors) {
+            ctClass.addConstructor(CtNewConstructor.make(constructor, ctClass));
+        }
+
+        // 添加属性
+        for (String field : fields) {
+            ctClass.addField(CtField.make(field, ctClass));
+        }
+
+        // 添加方法
+        for (String method : methods) {
+            ctClass.addMethod(CtNewMethod.make(method, ctClass));
+        }
+        return ctClass;
+    }
+
+    /**
+     * 根据类名,获取当前 CtClassBuilder 中包含的类全名
+     *
+     * @param className 类名
+     * @return 类全名
+     */
+    protected String getQualifiedClassName(String className) {
+        if (className.contains(".")) {
+            return className;
+        }
+        if (fullNames.containsKey(className)) {
+            return fullNames.get(className);
+        }
+        return ClassUtils.forName(imports.toArray(new String[0]), className).getName();
+    }
+}
--- component-common/src/main/java/org/lucas/compiler/support/JavassistCompiler.java
+++ component-common/src/main/java/org/lucas/compiler/support/JavassistCompiler.java
@@ -1,17 +1,9 @@
 package org.lucas.compiler.support;
 
-import javassist.ClassPool;
 import javassist.CtClass;
-import javassist.CtField;
-import javassist.CtNewConstructor;
-import javassist.CtNewMethod;
-import javassist.LoaderClassPath;
 import org.lucas.util.ClassUtils;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -59,96 +51,45 @@ public class JavassistCompiler extends AbstractCompiler {
 
     @Override
     protected Class<?> doCompile(final String name, final String source) throws Throwable {
-        int i = name.lastIndexOf('.');
-        String className = i < 0 ? name : name.substring(i + 1);
-        final ClassPool pool = new ClassPool(true);
-        // 添加当前的搜索路径
-        pool.appendClassPath(new LoaderClassPath(ClassUtils.getCallerClassLoader(getClass())));
-        Matcher matcher = IMPORT_PATTERN.matcher(source);
-        List<String> importPackages = new ArrayList<>();
-        // 类名 -> 类全名
-        Map<String, String> fullNames = new HashMap<>(16);
-        while (matcher.find()) {
-            // 导入包的全名
-            String pkg = matcher.group(1);
-            if (pkg.endsWith(".*")) {
-                // 导入的包名
-                String pkgName = pkg.substring(0, pkg.length() - 2);
-                // 添加到 ClassPool 中,在编译的时候搜索.
-                pool.importPackage(pkgName);
-                importPackages.add(pkgName);
-            } else {
-                int pi = pkg.lastIndexOf('.');
-                if (pi > 0) {
-                    // 导入的包名
-                    String pkgName = pkg.substring(0, pi);
-                    pool.importPackage(pkgName);
-                    importPackages.add(pkgName);
-                    // 导入的类名 pkg.substring(pi + 1)
-                    fullNames.put(pkg.substring(pi + 1), pkg);
-                }
-            }
-        }
-        String[] packages = importPackages.toArray(new String[0]);
+        final CtClassBuilder builder = new CtClassBuilder();
+        builder.setClassName(name);
 
-        matcher = EXTENDS_PATTERN.matcher(source);
-        CtClass cls;
-        if (matcher.find()) {
-            // 继承类
-            String extend = matcher.group(1).trim();
-            // 继承类全名
-            String extendClass;
-            if (extend.contains(ClassUtils.PACKAGE_SEPARATOR)) {
-                extendClass = extend;
-            } else if (fullNames.containsKey(extend)) {
-                extendClass = fullNames.get(extend);
-            } else {
-                extendClass = ClassUtils.forName(packages, extend).getName();
-            }
-            // 创建一个 public 修饰的类
-            cls = pool.makeClass(name, pool.get(extendClass));
-        } else {
-            cls = pool.makeClass(name);
+        // 处理导入类信息
+        Matcher matcher = IMPORT_PATTERN.matcher(source);
+        while (matcher.find()) {
+            builder.addImports(matcher.group(1).trim());
         }
+
+        // 处理扩展父类信息
+        matcher = EXTENDS_PATTERN.matcher(source);
+        if (matcher.find()) {
+            builder.setSuperClassName(matcher.group(1).trim());
+        }
+
+        // 处理实现接口信息
         matcher = IMPLEMENTS_PATTERN.matcher(source);
         if (matcher.find()) {
-            // ifaces: 接口
             String[] ifaces = matcher.group(1).trim().split("\\,");
-            for (String iface : ifaces) {
-                iface = iface.trim();
-                // 接口类全名
-                String ifaceClass;
-                if (iface.contains(ClassUtils.PACKAGE_SEPARATOR)) {
-                    ifaceClass = iface;
-                } else if (fullNames.containsKey(iface)) {
-                    ifaceClass = fullNames.get(iface);
-                } else {
-                    ifaceClass = ClassUtils.forName(packages, iface).getName();
-                }
-                // 将类添加接口
-                cls.addInterface(pool.get(ifaceClass));
-            }
+            Arrays.stream(ifaces).forEach(i -> builder.addInterface(i.trim()));
         }
 
-        // 获取代码体
-        String body = source.substring(source.indexOf("{") + 1, source.length() - 1);
-        // 根据修饰符对方法进行切割
+        // 处理构造函数,属性字段,方法信息.
+        String body = source.substring(source.indexOf('{') + 1, source.length() - 1);
         String[] methods = METHODS_PATTERN.split(body);
-        for (String method : methods) {
-            method = method.trim();
-            if (method.length() > 0) {
-                if (method.startsWith(className)) {
-                    // 添加构造方法
-                    cls.addConstructor(CtNewConstructor.make(PUBLIC_MODIFIER + method, cls));
-                } else if (FIELD_PATTERN.matcher(method).matches()) {
-                    // 添加字段
-                    cls.addField(CtField.make(PRIVATE_MODIFIER + method, cls));
-                } else {
-                    // 在类中根据源代码创建一个方法
-                    cls.addMethod(CtNewMethod.make(PUBLIC_MODIFIER + method, cls));
-                }
+        String className = ClassUtils.getShortName(name);
+        Arrays.stream(methods).map(String::trim).filter(m -> !m.isEmpty()).forEach(method -> {
+            if (method.startsWith(className)) {
+                builder.addConstructor("public " + method);
+            } else if (FIELD_PATTERN.matcher(method).matches()) {
+                builder.addField("private " + method);
+            } else {
+                builder.addMethod("public " + method);
             }
-        }
+        });
+
+        // 获取类加载器
+        ClassLoader classLoader = ClassUtils.getCallerClassLoader(getClass());
+        CtClass cls = builder.build(classLoader);
         // 通过类加载器,和权限域创建对象.
         return cls.toClass(ClassUtils.getCallerClassLoader(getClass()), JavassistCompiler.class.getProtectionDomain());
     }
