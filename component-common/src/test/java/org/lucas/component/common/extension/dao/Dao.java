594859e66aeb0adb949ebae380eb35f406e65737
zhouxu <zhouxu@wljs.so>
2019-07-04 14:31:11 +0800

    Commit

--- component-common/src/test/java/org/lucas/component/common/extension/dao/Dao.java
+++
@@ -1,6 +0,0 @@
-package org.lucas.component.common.extension.dao;
-
-public interface Dao {
-
-    public void update();
-}
--- component-common/src/test/java/org/lucas/component/common/extension/dao/impl/DaoImpl.java
+++
@@ -1,9 +0,0 @@
-package org.lucas.component.common.extension.dao.impl;
-
-import org.lucas.component.common.extension.dao.Dao;
-
-public class DaoImpl implements Dao {
-    public void update() {
-
-    }
-}
