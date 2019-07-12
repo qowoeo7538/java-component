1f027e3fd5c39e740b87d2e8c50aae3a399afcc3
zhouxu <zhouxu@wljs.so>
2019-07-12 16:44:30 +0800

    Commit

--- component-thread/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module component.thread {
-    requires spring.jcl;
-    requires component.common;
-    requires spring.core;
-    requires org.eclipse.collections.api;
-    requires org.eclipse.collections.impl;
-}
